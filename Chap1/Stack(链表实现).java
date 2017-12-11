5efb3365e02b7d6ea52a68e694480b75a6e90061
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-11 14:19:13 +0800

    stack的链表实现

---
+++ Chap1/Stack(链表实现).java
@@ -0,0 +1,42 @@
+public class Stack<Item> implements Iterable<Item>{
+    private Node first;
+    private int N;
+    //
+    private class Node{
+        Item item;
+        Node next;
+
+    }
+    public int size(){
+        return N;
+    }
+    public void push(Item item){
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+    public Item pop(){
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+    public Iterator<Item> iterator(){
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item>
+    {
+        private int i = N;
+        public boolean hasNext(){
+            return i > 0;
+        }
+        public Item next(){
+            return a[--i];
+        }
+        public void remove(){
+        }
+    }
+}
