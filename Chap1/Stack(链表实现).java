a5f85282f29b0abd55b3a3e6eabf73c132d461d2
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-11 14:59:39 +0800

    添加注释

--- Chap1/Stack(链表实现).java
+++ Chap1/Stack(链表实现).java
@@ -1,7 +1,7 @@
 public class Stack<Item> implements Iterable<Item>{
     private Node first;
     private int N;
-    //
+    //链表的一个节点，包含一个数据存储单元和一个指向下个元素的“指针”
     private class Node{
         Item item;
         Node next;
@@ -10,22 +10,32 @@ public class Stack<Item> implements Iterable<Item>{
     public boolean isEmpty(){
         return N == 0;
     }
+
     public int size(){
         return N;
     }
+
     public void push(Item item){
+        //保存原链表的头节点到oldfirst，保证整个链表不会丢失
         Node oldfirst = first;
+        //新建一个节点，并把first指向它
         first = new Node();
+        //把入栈的数据存储在新的节点first里
         first.item = item;
+        //把first的next指向oldfirst，也就是与原链表链接到一起
         first.next = oldfirst;
         N++;
     }
+
     public Item pop(){
+        //拷贝first节点数据
         Item item = first.item;
+        //把first节点的下一个节点指向first，相当于删除了first节点
         first = first.next;
         N--;
         return item;
     }
+
     public Iterator<Item> iterator(){
         return new ReverseArrayIterator();
     }
