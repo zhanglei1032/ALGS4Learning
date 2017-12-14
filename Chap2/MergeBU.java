6ce521c907e4875c3cf36aa7db5e1e8e29fb8dda
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-14 16:57:15 +0800

    添加自底向上归并排序的实现

---
+++ Chap2/MergeBU.java
@@ -0,0 +1,73 @@
+//
+public class MergeBU{
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz ) {
+            for (int lo = 0; lo < N - sz ; lo += sz + sz) {
+                merge(a, lo, lo+sz-1, Math.min(lo+sz+sz-1， N-1));
+            }
+        }
+        sort(a, 0, a.length-1);
+    }
+
+    //原地归并，借助aux数组辅助
+    public static void merge(Comparable[] a, int lo, int mid, int hi){
+        //i跟踪数组左半边，j跟踪数组右半边
+        int i = lo, j = mid + 1;
+        //复制一份到aux数组
+        for (int k = lo; k <= hi; k++ ) {
+            aux[k] = a[k];
+        }
+        for (int k = lo; k <= hi; k++ ) {
+            //此时左半边数组已经全部比较完，所以把右半边数组直接复制到a数组
+            if (i > mid) {
+                a[k] = aux[j++];
+            //此时右半边数组已经全部比较完，所以把左半边数组直接复制到a数组
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            //真正的比较操作，左右两边数组分别比较到了aux[i]和aux[j]，
+            //把较小的元素放入a数组,然后i或j向后移动
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a){
+        for (int i = 0; i < a.length ; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a){
+        for (int i = 1; i < a.length ; i++) {
+            if (less(a[i], a[i-1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
