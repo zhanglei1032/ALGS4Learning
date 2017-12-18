3b42fda936c15eca188764ae14d6ea4d32260f18
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-18 16:18:51 +0800

    添加注释

--- Chap3/SequentialSearchST.java
+++ Chap3/SequentialSearchST.java
@@ -13,20 +13,24 @@ public class SequentialSearchST<Key, Value>{
 
     public Value get(Key key){
         for (Node x = first; x != null; x = x.next ) {
+            //命中
             if (key.equeals(x.key)) {
                 return x.val
             }
+            //未命中
             return null;
         }
     }
 
     public void put (Key key, Value val){
         for (Node x = first; x != null ; x = x.next ) {
+            //命中，更新
             if (key.equeals(x.key)) {
                 x.val = val;
                 return;
             }
         }
+        //未命中，新建节点
         first = new Node(key, val, first);
     }
 }
