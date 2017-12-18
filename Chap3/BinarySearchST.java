d6a7eb1f371febe4634b1279a45e7e27ffb58e27
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-18 17:07:42 +0800

    基于有序数组的二分查找

---
+++ Chap3/BinarySearchST.java
@@ -0,0 +1,71 @@
+//基于有序数组的二分查找
+public class BinarySearchST<Key extends Comparable<Key>, Value>{
+    private Key[] keys;
+    private Value[] vals;
+    private int N;
+
+    public BinarySearchST(int capacity){
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public Value get(Key key){
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            return vals[i];
+        } else {
+            return null;
+        }
+    }
+
+    public int rank(Key key){
+        int lo = 0, hi = N - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) {
+                hi = mid - 1;
+            } else if( cmp > 0){
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return lo;
+    }
+
+    public void put (Key key, Value val){
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            vals[i] = val;
+            return;
+        }
+        for (int j = N; j > i ; j--) {
+            keys[i] = key[j-1];
+            vals[j] = val[j-1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
+    //delete为自己实现的
+    public void delete(Key key){
+        if (isEmpty()) {
+            return null;
+        }
+        int index = rank(key)
+        if ( index == 0) {
+            return null;
+        } else {
+           keys[i] = null;
+           vals[i] = null;
+        }
+    }
+}
