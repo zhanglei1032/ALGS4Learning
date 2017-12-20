940034d8990a7e745da36b72434cdd6a76eab854
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-20 10:03:05 +0800

    BST实现补充

--- Chap3/BST.java
+++ Chap3/BST.java
@@ -19,7 +19,7 @@ public class BST<Key extends Comparable<Key>, Value>{
         return siez(root);
     }
 
-    public int size(Node x){
+    private int size(Node x){
         if (x == null) {
             return 0;
         } else {
@@ -45,20 +45,22 @@ public class BST<Key extends Comparable<Key>, Value>{
         }
     }
 
-    private int rank(Key key){
-        int lo = 0, hi = N - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            int cmp = key.compareTo(keys[mid]);
-            if (cmp < 0) {
-                hi = mid - 1;
-            } else if( cmp > 0){
-                lo = mid + 1;
-            } else {
-                return mid;
-            }
+    public int rank(Key key){
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x){
+        if (x == null) {
+            return 0;
         }
-        return lo;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return rank(key, x.left);
+        } else if (cmp > 0) {
+            return 1 + size(x.left) + rank(key, x.right);
+        } else {
+            return size(x.left);
+        }
     }
 
     public void put (Key key, Value val){
@@ -97,7 +99,21 @@ public class BST<Key extends Comparable<Key>, Value>{
     }
 
     public Key select(int k){
-        return keys[k];
+        return select(root, k).key;
+    }
+
+    private Node select(Node x, int k){
+        if (x == null) {
+            return null;
+        }
+        int t = size(x.left);
+        if (t > k) {
+            return select(x.left, k);
+        } else if (t < k) {
+            return select(x.right, k-t-1);
+        } else {
+            return x;
+        }
     }
 
     public Key ceiling(Key key){
@@ -131,28 +147,72 @@ public class BST<Key extends Comparable<Key>, Value>{
         }
     }
 
-    //delete为自己实现的
-    public void delete(Key key){
-        if (isEmpty()) {
-            return null;
-        }
-        int index = rank(key)
-        if ( index == 0) {
-            return null;
-        } else {
-           keys[i] = null;
-           vals[i] = null;
-        }
+    public void deleteMin(){
+        root = deleteMin(root);
     }
 
-    public Iterable<key> keys(Key lo, Key hi){
+    private Node deleteMin(Node x){
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(Key key){
+       root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key){
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = delete(x.left, key);
+        } else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            if (x.right == null) {
+                return x.left;
+            }
+            if (x.left == null) {
+                return x.right;
+            }
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + -1;
+        return x;
+    }
+
+    public Iterable<Key> keys(){
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi){
         Queue<Key> q = new Queue<Key>();
-        for (int i = rank(lo); i < rank(hi) ; i++) {
-            q.enqueue(keys[i]);
-        }
-        if (contains(hi)) {
-            q.enqueue(keys[rank(hi)]);
-        }
+        keys(root, q, lo, hi);
         return q;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi){
+        if (x == null) {
+            return;
+        }
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) {
+            keys(x.left, queue, lo, hi);
+        }
+        if (cmplo <= 0 && cmphi >= 0) {
+            queue.enqueue(x.key);
+        }
+        if (cmphi > 0) {
+            keys(x.right, queue, lo, hi);
+        }
     }
 }
