ea8ba6b94b63f7028996f1d2a00a8b70e064faa9
zhanglei1032 <zhanglei1032@gmail.com>
2017-12-24 10:35:50 +0800

    添加unitfind实现

---
+++ Chap1/UF.java
@@ -0,0 +1,39 @@
+public class UF{
+    private int[] id;
+    private int count;
+
+    public UF(int N){
+        count = N;
+        id = new int [N];
+        for (int i = 0; i < N ; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int count(){
+        return count;
+    }
+
+    public boolean connected(int p, int q){
+        return find(p) == find(q);
+    }
+
+    public int find(int p)
+
+    public void union(int p, int q)
+
+    public static void main(String[] args){
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        while(!StdIn.isEmpty()){
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) {
+                continue;
+            }
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + "components");
+    }
+}
