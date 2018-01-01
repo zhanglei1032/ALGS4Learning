7746b012319835826482c999e5913b8533fd8bff
zhanglei1032 <1032465191@qq.com>
2018-01-01 10:44:24 +0800

    添加注释

--- Chap3/BST.java
+++ Chap3/BST.java
@@ -3,11 +3,11 @@ public class BST<Key extends Comparable<Key>, Value>{
     private Node root;
 
     private class Node{
-        private Key key;
-        private Value val;
-        private Node left, right;
-        private int N;
-
+        private Key key;//键
+        private Value val;//值
+        private Node left, right;//指向左右子树的链接
+        private int N;//以该节点为根的子树的节点数量
+        //构建一个二叉查找树节点，需要一个键、一个值、和子树节点的数量
         public Node(Key key, Value val, int N){
             this.key = key;
             this.val = val;
@@ -16,7 +16,7 @@ public class BST<Key extends Comparable<Key>, Value>{
     }
 
     public int size(){
-        return siez(root);
+        return size(root);
     }
 
     private int size(Node x){
@@ -30,7 +30,7 @@ public class BST<Key extends Comparable<Key>, Value>{
     public Value get(Key key){
         return get(root, key);
     }
-
+    //在以x为根节点的子树中查找并返回key对应的值，没有返回null
     private Value get(Node x, Key key){
         if (x == null) {
             return null;
@@ -48,8 +48,11 @@ public class BST<Key extends Comparable<Key>, Value>{
     public void put (Key key, Value val){
         root = put(root, key, val);
     }
-
+    //如果key存在于以x为根节点的子树中则更新它的值；
+    //否则将以key和val为键值对的新的节点插入到该子树中
     private Node put (Node x, Key key, Value val){
+        //如果x为null，返回一个新建节点
+        //当传入的root为null时，返回的新节点将作为新树的根
         if (x == null) {
             return new Node(key, val, 1);
         }
@@ -61,6 +64,7 @@ public class BST<Key extends Comparable<Key>, Value>{
         } else {
             x.val = val;
         }
+        //沿着搜索路径向上增加节点计数器的值
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
@@ -68,7 +72,7 @@ public class BST<Key extends Comparable<Key>, Value>{
     public Key min(){
         return min(root).key;
     }
-
+    //在左子树中递归查找最小值
     private Node min(Node x){
         if (x.left == null) {
             return x;
