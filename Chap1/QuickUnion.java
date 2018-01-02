c212caf1a3643677ced476374a982b83fa0572c9
zhanglei1032 <zhanglei1032@gmail.com>
2018-01-02 10:54:49 +0800

    添加注释

--- Chap1/QuickUnion.java
+++ Chap1/QuickUnion.java
@@ -2,6 +2,7 @@ public class QuickUnion{
     private int[] id;
     private int count;
 
+    //初始化分量id数组，长度为元素个数
     public QuickUnion(int N){
         count = N;
         id = new int [N];
@@ -17,7 +18,7 @@ public class QuickUnion{
     public boolean connected(int p, int q){
         return find(p) == find(q);
     }
-
+    //因为只有根节点满足p=id[p]，所以find返回的是p的根节点
     public int find(int p){
         while (p != id[p]){
             p = id[p];
@@ -28,9 +29,11 @@ public class QuickUnion{
     public void union(int p, int q){
         int pRoot = find(p);
         int qRoot = find(q);
+        //pq具有相同根节点，在同一个树上，已经连通，函数返回
         if (pRoot == qRoot) {
             return;
         }
+        //pq不连通，把q的根节点赋给id[pRoot]，pRoot成为qRoot的子树
         id[pRoot] = qRoot;
         count--;
     }
@@ -41,6 +44,7 @@ public class QuickUnion{
         while(!StdIn.isEmpty()){
             int p = StdIn.readInt();
             int q = StdIn.readInt();
+            //pq连通，跳出本次循环
             if (uf.connected(p, q)) {
                 continue;
             }
